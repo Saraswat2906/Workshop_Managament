4abc1135ad671d6a6294c9f874a1bb47e2a7c90a
Saraswat2906 <142869082+Saraswat2906@users.noreply.github.com>
2025-12-19 21:07:06 +0530

    Update Database_config.py

--- Database_config.py
+++ Database_config.py
@@ -2,7 +2,7 @@ import os
 # Setting up the postgressql connection string here
 #Format: "postgresql://USERNAME:PASSWORD@localhost/DATABASE_NAME"
 # Database_url="postgresql://postgres:Ada@6402628@localhost/Workshop Management"
-Database_url = "postgresql://postgres:Ada%406402628@localhost/Workshop Management"
+
 
 class config:
     SQLALCHEMY_DATABASE_URI = Database_url
